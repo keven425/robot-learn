e43d61fed635e5237a590257697805f9b2e16a08
Keven Wang <keven425@gmail.com>
2017-10-20 14:24:41 -0700

    consolidated run functions

--- rl/config.py
+++ rl/config.py
@@ -1,7 +1,12 @@
 class Config():
 
   def __init__(self):
+    # atari config
+    from ppo.cnn_policy import CnnPolicy
+    num_frames = 40e6
+    self.num_timesteps = int(num_frames / 4 * 1.1)
     self.timesteps_per_batch = 256
+    self.policy = CnnPolicy
     self.clip_param = 0.2
     self.entcoeff = 0.01
     self.optim_epochs = 4
@@ -10,3 +15,24 @@ class Config():
     self.gamma = 0.99
     self.lam = 0.95
     self.schedule = 'linear'
+    self.wrap_env_fn = wrap_train
+
+    # mujoco config
+    # from ppo.ppo_rl import MlpPolicy
+    # self.num_timesteps = 1e6
+    # self.timesteps_per_batch = 2048
+    # self.policy = MlpPolicy
+    # self.clip_param = 0.2
+    # self.entcoeff = 0.0,
+    # self.optim_epochs = 10
+    # self.optim_stepsize = 3e-4
+    # self.optim_batchsize = 64
+    # self.gamma = 0.99
+    # self.lam = 0.95
+    # self.schedule = 'linear'
+
+def wrap_train(env):
+  from common.atari_wrappers import (wrap_deepmind, FrameStack)
+  env = wrap_deepmind(env, clip_rewards=True)
+  env = FrameStack(env, 4)
+  return env
--- rl/ppo/run.py
+++ rl/ppo/run.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python
 
 import bench
-import os
 import os.path as osp
 import gym, logging
 from common import logger
 from config import Config
-from ppo.cnn_policy import CnnPolicy
 
-def wrap_train(env):
-    from common.atari_wrappers import (wrap_deepmind, FrameStack)
-    env = wrap_deepmind(env, clip_rewards=True)
-    env = FrameStack(env, 4)
-    return env
 
-def train(env_id, gpu, num_frames, config):
+def train(env_id, gpu, num_timesteps, config):
     from ppo.ppo_rl import PPO
     env = gym.make(env_id)
     env = bench.Monitor(env, logger.get_dir() and osp.join(logger.get_dir(), "monitor.json"))
     gym.logger.setLevel(logging.WARN)
-    env = wrap_train(env)
-    num_timesteps = int(num_frames / 4 * 1.1)
+    if hasattr(config, 'wrap_env_fn'):
+      env = config.wrap_env_fn(env)
     ppo_rl = PPO(env,
                  gpu=gpu,
-                 policy=CnnPolicy,
+                 policy=config.policy,
                  timesteps_per_batch=config.timesteps_per_batch,
                  clip_param=config.clip_param,
                  entcoeff=config.entcoeff,
@@ -47,7 +40,7 @@ def main():
     args = parser.parse_args()
     logger.configure(args.log)
     config = Config()
-    train(args.env, args.gpu, num_frames=40e6, config=config)
+    train(args.env, args.gpu, num_timesteps=config.num_timesteps, config=config)
 
 if __name__ == '__main__':
     main()
