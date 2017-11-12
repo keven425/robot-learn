645a208173b4a35cf90d5204c5db261c2a6347de
Keven Wang <keven425@gmail.com>
2017-11-11 17:59:48 -0800

    updated abb xml

--- rl/environment/push_object.py
+++ rl/environment/push_object.py
@@ -393,7 +393,7 @@ if __name__ == '__main__':
             env.render()
         for i in range(5000):
             # env.step([1., 1., 1., 1., 1., 1.])
-            env.step([0., 1., 1., 0., 0., 0.])
+            env.step([0., 0., 1., 0., 0., 0.])
             env.render()
         for i in range(5000):
             # env.step([-1., -1., -1., -1., -1., -1.])
