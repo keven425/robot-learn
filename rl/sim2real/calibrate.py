a0536fd7877e954f97a1839de38edc055fa093b1
Keven Wang <keven425@gmail.com>
2017-11-18 12:37:50 -0800

    added calibration script

---
+++ rl/sim2real/calibrate.py
@@ -0,0 +1,25 @@
+import time
+from rl.environment.push_object import PushObjectEnv
+from arm.control import Arm
+
+
+def act(env, command):
+  # move both environment and real robot
+  ob, _, done, _ = env.step(command)
+  env.render()
+  # arm.set_positions(command)
+
+
+if __name__ == '__main__':
+  env = PushObjectEnv(frame_skip=1)
+  env.reset(rand_init_pos=False)
+  # arm = Arm()
+
+  for i in range(1500):
+    command = [0., 0., 0., 0., 0., 0.]
+    act(env, command)
+    time.sleep(0.5)
+
+
+
+
