8470f710a3b939a26ff79b0ab04d7c82849f18ec
Yong Li <yonglisc@gmail.com>
2017-11-18 15:45:57 -0800

    serial testing

--- rl/arm/control.py
+++ rl/arm/control.py
@@ -8,7 +8,7 @@ import numpy as np
 from arm.denormalizer import Denormalizer
 
 # usbport = '/dev/ttyACM2'
-usbport = '/dev/cu.usbmodem1421'
+# usbport = '/dev/cu.usbmodem1421'
 
 # Set up serial baud rate
 # ser = serial.Serial(usbport, 9600, timeout=1)
@@ -48,6 +48,11 @@ class Arm(object):
         try:
             self.dev = serial.Serial(self.port, 9600, timeout=5, parity=serial.PARITY_NONE,
                                 stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+            for i in range(100):
+                self.dev.write(bytes([0]))
+                time.sleep(0.03)
+            self.dev.write(bytes([254]))
+            print('wrote 254')
         except:
             sys.exit("Error connecting device")
 
@@ -57,6 +62,7 @@ class Arm(object):
         line = bytearray()
         while True:
             c = self.dev.read(1)
+            print('c: ' + str(c))
             if c:
                 line += c
                 if line[-leneol:] == eol:
@@ -65,19 +71,22 @@ class Arm(object):
                 break
         return bytes(line)
 
-    def get_position(self, _, run_event):
-        while run_event.is_set():
-            try:
-                pos = _readline()
-                poses = pos.replace('\n', '').split(',')
-                print(poses)
-                if (len(poses) == 6):
-                    self.feedback = poses
-                    print("Position from arduino: ", self.feedback)
-                pass
-            except ValueError as ve:
-                print(ve)
-                pass
+    # def get_position(self, _, run_event):
+    def get_positions(self):
+        self.feedback = []
+        try:
+            pos = self._readline()
+            print(pos)
+            poses = pos.replace('\n', '').split(',')
+            print(poses)
+            if (len(poses) == 6):
+                self.feedback = poses
+                print("Position from arduino: ", self.feedback)
+                return self.feedback
+            pass
+        except ValueError as ve:
+            print(ve)
+            pass
 
     def move(self, servo, angle):
         if (0 <= angle <= 180):
@@ -90,45 +99,63 @@ class Arm(object):
         else:
             print("Servo angle must be an integer between 0 and 180.\n")
 
+    def move_all(self, angles):
+        # time.sleep(0.01)
+        self.dev.reset_input_buffer()
+        self.dev.write(bytes([255]))
+        assert(len(angles) == 6)
+        assert(np.all(angles <= 180))
+        assert(np.all(angles >= 0))
+
+        for i, angle in enumerate(angles):
+            angle = int(angle)
+            print('servo %d angle %d' % (i, angle))
+            self.dev.write(bytes([angle]))
+
     def set_positions(self, angles):
         denormalizer = Denormalizer(RANGES)
         denormalized = denormalizer.denormalize(angles)
         time.sleep(0.01)
-        for idx, angle in enumerate(denormalized):
-            angle = int(angle)
-            print(idx+1, angle)
-            servo = idx + 1
-            self.move(servo, angle)
+        self.move_all(denormalized)
+        # while len(self.feedback) != 6:
+        #     self.get_position()
+        #     pass
+        # return self.feedback
+        # for idx, angle in enumerate(denormalized):
+        #     angle = int(angle)
+        #     print(idx+1, angle)
+        #     servo = idx + 1
+        #     self.move(servo, angle)
 
-    def home(self):
-        time.sleep(0.1)
-        self.move(1,30)
-        time.sleep(0.03)
-        self.move(1,120)
-        time.sleep(0.03)
-        self.move(1,30)
-        time.sleep(0.03)
-        time.sleep(0.5)
-        self.move(1,120)
-        # time.sleep(0.5)
-        self.move(2,70)
-        # time.sleep(0.5)
-        self.move(3,70)
-        # time.sleep(0.5)
-        self.move(4,70)
-        # time.sleep(0.5)
-        self.move(5,70)
-        # time.sleep(0.5)
-        self.move(6,70)
-        time.sleep(0.5)
+    # def home(self):
+    #     time.sleep(0.1)
+    #     self.move(1,30)
+    #     time.sleep(0.03)
+    #     self.move(1,120)
+    #     time.sleep(0.03)
+    #     self.move(1,30)
+    #     time.sleep(0.03)
+    #     time.sleep(0.5)
+    #     self.move(1,120)
+    #     # time.sleep(0.5)
+    #     self.move(2,70)
+    #     # time.sleep(0.5)
+    #     self.move(3,70)
+    #     # time.sleep(0.5)
+    #     self.move(4,70)
+    #     # time.sleep(0.5)
+    #     self.move(5,70)
+    #     # time.sleep(0.5)
+    #     self.move(6,70)
+    #     time.sleep(0.5)
 
-if __name__ == '__main__':
-    arm = Arm()
+# if __name__ == '__main__':
+#     arm = Arm()
 
-    positions = np.array([ -0.11467527, -0.16472031, 0.19183921, 0.17777404, 0.16154106, 0.18859741])
+#     positions = np.array([ -0.11467527, -0.16472031, 0.19183921, 0.17777404, 0.16154106, 0.18859741])
 
-    # print(denormalized)
-    arm.set_positions(positions)
+#     # print(denormalized)
+#     arm.set_positions(positions)
 
 
 
--- rl/sim2real/calibrate.py
+++ rl/sim2real/calibrate.py
@@ -1,23 +1,26 @@
 import time
+import numpy as np
 from environment.push_object import PushObjectEnv
 from arm.control import Arm
 
 
-def act(env, command):
+def act(env, arm, command):
   # move both environment and real robot
-  ob, _, done, _ = env.step(command)
-  env.render()
-  # arm.set_positions(command)
+  # ob, _, done, _ = env.step(command)
+  # env.render()
+  arm.set_positions(command)
+  angles = arm.get_positions()
+  print(angles)
 
 
 if __name__ == '__main__':
   env = PushObjectEnv(frame_skip=1)
   env.reset(rand_init_pos=False)
-  # arm = Arm()
+  arm = Arm()
 
   for i in range(1500):
-    command = [0., 0., 0., 0., 0., 0.]
-    act(env, command)
+    command = np.array([0.5, 0.5, 0., 0., 0., 0.])
+    act(env, arm, command)
     time.sleep(0.5)
 
 
