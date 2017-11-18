9fe23fbc846afd14e39648a90bad4251408e915b
Keven Wang <keven425@gmail.com>
2017-11-17 17:38:29 -0800

    added back endeff com to hidden ob

--- rl/environment/push_object.py
+++ rl/environment/push_object.py
@@ -153,9 +153,10 @@ class PushObjectEnv(utils.EzPickle):
 
     def get_hidden_ob(self):
         cube_com = self.get_body_com(self.obj_name)[:2]
-        # endeff_com = self.get_body_com(self.endeff_name)[:2]
+        endeff_com = self.get_body_com(self.endeff_name)[:2]
         return np.concatenate([
-            cube_com
+            cube_com,
+            endeff_com
         ])
 
 
