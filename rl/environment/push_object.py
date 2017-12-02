f87672af40927c6628539fe92a42f708a32c737a
Keven Wang <keven425@gmail.com>
2017-12-02 12:19:56 -0800

    added endeff euler to input state

--- rl/environment/push_object.py
+++ rl/environment/push_object.py
@@ -408,15 +408,46 @@ class PushObjectEnv(utils.EzPickle):
         # normalize pos
         cube_com = self.get_body_com(self.obj_name)
         endeff_com = self.get_body_com(self.endeff_name)
-        endeff_rotmat = self.get_body_xmat(self.endeff_name)
-        endeff_quat = self.get_body_xquat(self.endeff_name)
+        endeff_rotmat = self.get_body_xmat(self.endeff_name).reshape([3, 3])
+        endeff_euler = rotationMatrixToEulerAngles(endeff_rotmat)
 
         return np.concatenate([
             cube_com,
             endeff_com,
-            endeff_rotmat,
-            endeff_quat
+            endeff_euler,
+            np.sin(endeff_euler),
+            np.cos(endeff_euler)
         ])
+
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R):
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
+def rotationMatrixToEulerAngles(R):
+
+    assert (isRotationMatrix(R))
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
 
 
 # Separate Process to save video. This way visualization is
